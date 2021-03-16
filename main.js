9d5fec911d58de4febfc54e820c4d3b27a017aa7
Steven Moyano <stevenmoyano1@gmail.com>
2021-03-16 11:50:53 -0500

    Added js to html

---
+++ main.js
@@ -0,0 +1,53 @@
+const spanMess = document.querySelector("span.mess-with-me");
+spanMess.style.fontSize = "40px";
+
+const pMess = document.querySelector("p.mess-with-me");
+pMess.style.backgroundColor = "green";
+
+const removeImg = document.querySelector("#hide-me");
+removeImg.style.display = "none";
+
+const firstImg = document.querySelector("#triceratops");
+firstImg.style.width = "324px";
+
+function changeBackgroundToOrange() {
+    spanMess.style.color = "orange";
+}
+
+spanMess.addEventListener("click", changeBackgroundToOrange);
+
+function addRedBorder() {
+    firstImg.style.border = "1px solid red";
+}
+
+firstImg.addEventListener("click", addRedBorder);
+
+const featheredDino = document.querySelector("#feathers");
+
+function transparent() {
+    featheredDino.style.opacity = ".5";
+}
+
+featheredDino.addEventListener("click", transparent);
+
+const switchButton = document.querySelector("#toggle");
+
+function changeBackgroundToBlack() {
+    const dinoRow = document.querySelector("#row");
+    dinoRow.style.backgroundColor = "black";
+}
+
+switchButton.addEventListener("click", changeBackgroundToBlack);
+
+const bigDino = document.querySelector("#biggify");
+
+function enlarge() {
+    bigDino.style.width = "200px";
+}
+
+function regularSize() {
+    bigDino.style.width = "162px";
+}
+
+bigDino.addEventListener("mouseover", enlarge);
+bigDino.addEventListener("mouseout", regularSize);
